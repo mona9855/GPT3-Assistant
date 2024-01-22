0c06433431960c46156b99ef7f0df9a2926d91d9
mona9855 <mona.magdy2012912012911@gmail.com>
2024-01-22 14:59:28 +0200

    changing toggle menu links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,19 +3,21 @@ import { RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 
-const Menu = () => {
-  return(
-  <>
-    <p><a href='#home'>Home</a></p>
-    <p><a href='#wgpt3'>What is GPT?</a></p>
-    <p><a href='#possibility'>Open AI</a></p>
-    <p><a href='#features'>Case Studies</a></p>
-    <p><a href='#blog'>Library</a></p>
-  </>)
-}
+
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+
+  const Menu = () => {
+    return(
+    <>
+      <p onClick={() => setToggleMenu(false)}><a href='#home'>Home</a></p>
+      <p onClick={() => setToggleMenu(false)}><a href='#wgpt3'>What is GPT?</a></p>
+      <p onClick={() => setToggleMenu(false)}><a href='#possibility'>Open AI</a></p>
+      <p onClick={() => setToggleMenu(false)}><a href='#features'>Case Studies</a></p>
+      <p onClick={() => setToggleMenu(false)}><a href='#blog'>Library</a></p>
+    </>)
+  }
 
   return (
     <div className='gpt3__navbar'>
